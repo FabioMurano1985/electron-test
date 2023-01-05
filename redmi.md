578f2ada04b9428ed262f1d879b435090d75554f
Murano <fabio.murano@accenture.com>
2023-01-05 18:48:33 +0100

    fix varie

--- redmi.md
+++ redmi.md
@@ -2,3 +2,14 @@
 
 
 # per configurazione webpack (funzionante ma non in uso dall'applicazione leggere nei commenti)
+
+# check update logs
+C:\Users\{nomeutente}\AppData\Roaming\Smartapp\logs\main.lo
+
+# per effettuare un rilascio
+cambiare la versione nel package.json es 1.0.1
+preparare in draft il tag su github con la versione che stiamo per rilasciare 1.0.1
+effettuiamo il rilascio da terminale "npm run release" (non dimenticandoci di aggiungere il token github)
+set GH_TOKEN  su windows
+export GH_TOKEN su mac
+
--- src/updater.js
+++ src/updater.js
@@ -1,10 +1,57 @@
+const { dialog } = require("electron");
 const { autoUpdater } = require("electron-updater");
 
-module.exports=()=>{
-    const log = require("electron-log")
-    log.transports.file.level = "debug"
-    autoUpdater.logger = log
-    console.log('check update...')
-      autoUpdater.checkForUpdates()
+module.exports = () => {
+  const log = require("electron-log")
+  log.transports.file.level = "debug"
+  autoUpdater.logger = log
+  autoUpdater.autoDownload = false;
 
+  console.log('check update...')
+  autoUpdater.checkForUpdates()
+
+  autoUpdater.on('update-available', () => {
+
+    dialog.showMessageBox({
+      type: 'info',
+      title: 'Update avaible',
+      title: 'Update avaible',
+      message: "A new version of Smartapp is Avaible. Do you want to update now?",
+      buttons: [
+        'Update', 'no'
+      ]
+    }).then((res) => {
+      let buttonIndex = res.response;
+
+      if (buttonIndex == 0) {
+        autoUpdater.downloadUpdate()
+      }
+    })
+
+    autoUpdater.on('update-downloaded', () => {
+
+      dialog.showMessageBox({
+        type: 'info',
+        title: 'Update ready',
+        title: 'Update avaible',
+        message: "Install & restart now?",
+        buttons: [
+          'Yes', 'Later'
+        ]
+      }).then((res) => {
+        let buttonIndex = res.response;
+
+        if (buttonIndex == 0) {
+          autoUpdater.quitAndInstall(false, true);
+        }
+
+
+      })
+
+    })
+
+
+  })
 }
+
+
